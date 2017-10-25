fd10e22778d148f9dc9251e5d9ea37092b7a0ce8
Moreno Gentili <brightsoul@tiscali.it>
2017-10-25 22:34:39 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace LedSample
+{
+    class Program
+    {
+        private static readonly GpioPin ledPin = Pi.Gpio.Pin00; //led (ouput)
+        private static readonly GpioPin buttonPin = Pi.Gpio.Pin07; //push button (input)
+
+        static void Main(string[] args)
+        {
+            ledPin.PinMode = GpioPinDriveMode.Output;
+            buttonPin.PinMode = GpioPinDriveMode.Input;
+            buttonPin.InputPullMode = GpioPinResistorPullMode.PullDown;
+
+            Console.WriteLine("Hello .NET Core on Raspberry PI!");
+            while(true)
+            {
+                ledPin.Write(buttonPin.ReadValue());
+                Thread.Sleep(100);
+            }
+            //for (var i = 0; i <5; i++)
+            //{
+            //    ledPin.Write(GpioPinValue.High);
+            //    Thread.Sleep(500);
+            //    ledPin.Write(GpioPinValue.Low);
+            //    Thread.Sleep(500);
+            //}
+
+
+            Console.WriteLine("Program complete!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LedSample
+This sample uses a .NET Core application to turn on a LED when a push button is pressed.
+
+![Wiring](wiring.png)
